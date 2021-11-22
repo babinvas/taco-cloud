64e1c297cc576cd9b6c40a5999bce3897491dde9
babinvas <babinvas@gmail.com>
2021-11-22 18:42:57 +0300

    Changed the ingredient class, the taco class and the order class to implement the JPA

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -3,17 +3,26 @@ package tacos;
 import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
 
+import javax.persistence.*;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Data
-public class Order {
+@Entity
+@Table(name = "Taco_Order")
+public class Order implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+
 	private Date placedAt;
 
 	@NotBlank(message = "Name is required")
@@ -44,5 +53,10 @@ public class Order {
 
 	public void addDesign(Taco design) {
 		this.tacos.add(design);
+	}
+
+	@PrePersist
+	void placedAt() {
+		this.placedAt = new Date();
 	}
 }
--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -2,15 +2,20 @@ package tacos;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Date;
 import java.util.List;
 
 @Data
+@Entity
 public class Taco {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+
 	private Date createdAt;
 
 	@NotNull
@@ -18,5 +23,10 @@ public class Taco {
 	private String name;
 
 	@Size(min = 1, message = "You must choose at least 1 ingredient")
-	private List<String> ingredients;
+	private List<Ingredient> ingredients;
+
+	@PrePersist
+	void createdAt() {
+		this.createdAt = new Date();
+	}
 }
