0b46459b435e710b06fcea8681989dace5ced974
babinvas <babinvas@gmail.com>
2021-12-01 20:15:57 +0300

    Changed the security config class

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package tacos.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,20 +12,31 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.anyRequest()
+					.fullyAuthenticated()
+				.and()
+				.formLogin();
+	}
+
+	@Override
 	protected void configure(AuthenticationManagerBuilder auth)
 			throws Exception {
 
 		auth
 			.ldapAuthentication()
-			.userSearchBase("ou=people")
-			.userSearchFilter("(uid={0})")
-			.groupSearchBase("ou=groups")
-			.groupSearchFilter("member={0}")
-			.passwordCompare()
-			.passwordEncoder(new BCryptPasswordEncoder())
-			.passwordAttribute("passcode")
-			.contextSource()
-				.root("dc=tacocloud,dc=com")
-				.ldif("classpath:users.ldif");
+				.userSearchBase("ou=people")
+				.userSearchFilter("(uid={0})")
+				.groupSearchBase("ou=groups")
+				.groupSearchFilter("member={0}")
+				.contextSource()
+					.root("dc=tacocloud,dc=com")
+					.ldif("classpath:users.ldif")
+				.and()
+				.passwordCompare()
+					.passwordEncoder(new BCryptPasswordEncoder())
+					.passwordAttribute("userPassword");
 	}
 }
