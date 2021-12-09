e1c3468e03c5789f59b17c97ae08e639d0afe566
babinvas <babinvas@gmail.com>
2021-12-09 21:17:14 +0300

    Changed the security config class

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -1,42 +1,50 @@
 package tacos.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private UserDetailsService userDetailsService;
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.anyRequest()
-					.fullyAuthenticated()
+				.antMatchers("/design", "/orders")
+					.access("hasRole('ROLE_USER')")
+				.antMatchers("/", "/**")
+					.access("permitAll")
+
 				.and()
-				.formLogin();
+					.formLogin()
+						.loginPage("/login")
+
+				.and()
+					.logout()
+						.logoutSuccessUrl("/");
+	}
+
+	@Bean
+	public PasswordEncoder encoder() {
+		return new StandardPasswordEncoder("53cr3t");
 	}
 
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth)
-			throws Exception {
-
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth
-			.ldapAuthentication()
-				.userSearchBase("ou=people")
-				.userSearchFilter("(uid={0})")
-				.groupSearchBase("ou=groups")
-				.groupSearchFilter("member={0}")
-				.contextSource()
-					.root("dc=tacocloud,dc=com")
-					.ldif("classpath:users.ldif")
-				.and()
-				.passwordCompare()
-					.passwordEncoder(new BCryptPasswordEncoder())
-					.passwordAttribute("userPassword");
+			.userDetailsService(userDetailsService)
+			.passwordEncoder(encoder());
 	}
 }
