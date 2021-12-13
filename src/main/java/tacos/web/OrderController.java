974bd365ab453fcd6a0fd4dcf5d382b6e67b91f0
babinvas <babinvas@gmail.com>
2021-12-13 18:43:45 +0300

    Changed the order controller class

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -2,6 +2,7 @@ package tacos.web;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import tacos.Order;
+import tacos.User;
 import tacos.data.OrderRepository;
 
 import javax.validation.Valid;
@@ -34,11 +36,14 @@ public class OrderController {
 
 	@PostMapping
 	public String processOrder(@Valid Order order, Errors errors,
-	                           SessionStatus sessionStatus) {
+	                           SessionStatus sessionStatus,
+	                           @AuthenticationPrincipal User user) {
 
 		if (errors.hasErrors()) {
 			return "orderForm";
 		}
+
+		order.setUser(user);
 
 		orderRepo.save(order);
 		sessionStatus.setComplete();
