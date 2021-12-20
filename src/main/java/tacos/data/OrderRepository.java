0d6b3d56ca3c02326740a24e29fad17f5b56bc40
babinvas <babinvas@gmail.com>
2021-12-20 18:14:03 +0300

    Changed the order repository and the order controller classes to display the user's latest orders

--- src/main/java/tacos/data/OrderRepository.java
+++ src/main/java/tacos/data/OrderRepository.java
@@ -2,6 +2,11 @@ package tacos.data;
 
 import org.springframework.data.repository.CrudRepository;
 import tacos.Order;
+import tacos.User;
+
+import java.util.List;
 
 public interface OrderRepository extends CrudRepository<Order, Long> {
+
+	List<Order> findByUserOrderByPlacedAtDesc(User user);
 }
--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
@@ -49,10 +50,18 @@ public class OrderController {
 		return "orderForm";
 	}
 
+	@GetMapping
+	public String ordersForUser(@AuthenticationPrincipal User user, Model model) {
+
+		model.addAttribute("orders", orderRepo.findByUserOrderByPlacedAtDesc(user));
+
+		return "orderList";
+	}
+
 	@PostMapping
 	public String processOrder(@Valid Order order, Errors errors,
-	                           SessionStatus sessionStatus,
-	                           @AuthenticationPrincipal User user) {
+	                            SessionStatus sessionStatus,
+	                            @AuthenticationPrincipal User user) {
 
 		if (errors.hasErrors()) {
 			return "orderForm";
